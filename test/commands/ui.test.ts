77239e0590c3487eb52b15456be53addcc558253
davetech <davidrums65@gmail.com>
2024-02-06 21:28:09 +0100

    feat(tests): add test files

---
+++ test/commands/ui.test.ts
@@ -0,0 +1,17 @@
+import {expect, test} from '@oclif/test'
+
+describe('ui', () => {
+  test
+  .stdout()
+  .command(['ui'])
+  .it('runs hello', ctx => {
+    expect(ctx.stdout).to.contain('hello world')
+  })
+
+  test
+  .stdout()
+  .command(['ui', '--name', 'jeff'])
+  .it('runs hello --name jeff', ctx => {
+    expect(ctx.stdout).to.contain('hello jeff')
+  })
+})
